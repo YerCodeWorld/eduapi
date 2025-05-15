d5caa24b82c45d33fc59d00efb58aed856f31be1
YerCodeWorld <yahiradolfo37@gmail.com>
2025-05-15 10:26:30 -0400

    Stop tracking ignored folders

--- dist/DbClient/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as prisma } from './src/prisma';
-export * from './src/types';
-export * from '@prisma/client';
-//# sourceMappingURL=index.d.ts.map
--- dist/DbClient/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as prisma } from './prisma';
-export * from '@prisma/client';
-//# sourceMappingURL=index.d.ts.map
